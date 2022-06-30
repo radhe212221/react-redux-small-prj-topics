3ef307336d97dc5103c9a3e6d0bbd9b93ec35799
radhe212221 <radhe212221@gmail.com>
2022-06-30 21:39:12 +0530

    2 files modified

--- src/comp/Heading.js
+++ src/comp/Heading.js
@@ -3,5 +3,18 @@ import { useSelector } from 'react-redux';
 export default function Heading() {
   const state = useSelector((s) => s);
   const { a } = state;
-  return <h1>All topics : {a?.length || 0} </h1>;
+  const calc = (num) => {
+    if (num === 1) {
+      return 'lime';
+    }
+    if (num === 0) {
+      return 'tomato';
+    }
+  };
+
+  return (
+    <h1 style={{ color: calc(state?.status) }}>
+      All topics : {a?.length || 0}{' '}
+    </h1>
+  );
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,14 +1,15 @@
 const intialstate = {
   a: [],
+  status: -1,
 };
 
 function reducer(state = intialstate, action) {
   console.log(action?.payload);
   switch (action.type) {
     case 'insert':
-      return { ...state, a: action.payload };
+      return { ...state, a: action.payload, status: 1 };
     case 'delete':
-      return { ...state, a: action.payload };
+      return { ...state, a: action.payload, status: 0 };
     default:
       return state;
   }
