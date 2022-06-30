2f7c558db1c897ce24e61bf81f840ccc707cb317
radhe212221 <radhe212221@gmail.com>
2022-06-30 21:48:13 +0530

    added autoficus

--- src/comp/Insert.js
+++ src/comp/Insert.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 export default function Insert() {
   const state = useSelector((s) => s);
   const dispatch = useDispatch();
+  const ref = React.useRef();
   const [name, setname] = React.useState('');
   const insert = () => {
     const condtition1 = state?.a?.some((x) => x === name?.trim()) === false;
@@ -11,15 +12,23 @@ export default function Insert() {
       let payload = [...state.a, name?.trim()];
       dispatch({ type: 'insert', payload });
     }
+
+    ref.current.focus();
   };
   const handleChange = (e) => {
     let { value } = e.target;
     setname(value);
   };
+
+  const boot = () => {
+    document.title = name;
+  };
+  React.useEffect(boot, [name]);
   return (
     <div>
       <h1>Insert-Form</h1>
       <input
+        ref={ref}
         name="name"
         placeholder="name"
         value={name}
